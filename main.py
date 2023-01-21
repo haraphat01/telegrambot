f9276dbb9f8290438e88a5406f6741e03c1aa308
haraphat01 <haraphat@gmail.com>
2023-01-21 19:17:59 +0100

    Added unique users to the dabase

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ import requests
 import openai
 import time
 from dotenv import load_dotenv
+import pymongo
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 os.getenv("TELEGRAM_API_KEY")
+os.getenv("MONGO_DB")
+
+
+
+# Connect to MongoDB
+client = pymongo.MongoClient(format(os.getenv("MONGO_DB")))
+db = client["abekeapo"]
+collection = db["Cluster0"]
 
 
 update_url = "https://api.telegram.org/bot{}/getUpdates".format(os.getenv("TELEGRAM_API_KEY"))
 send_message_url = "https://api.telegram.org/bot{}/sendMessage".format(os.getenv("TELEGRAM_API_KEY"))
 
 
+# Set to keep track of unique users
+unique_users = set()
 last_update_id = 0
 # Function to handle incoming messages
 def handle_message(update):
@@ -20,9 +31,15 @@ def handle_message(update):
         if "text" in message:
             message_text = message["text"]
             chat_id = message["chat"]["id"]
+            if chat_id not in unique_users:
+                unique_users.add(chat_id)
+                # Send a POST request to the Airtable API to add a new row
+                collection.insert_one({
+                "chat_id": chat_id
+                })
             model = "text-davinci-003"
-            temperature = 0.7
-            max_tokens = 256
+            temperature = 0.8
+            max_tokens = 1250
             top_p = 1
             frequency_penalty = 0
             presence_penalty = 0
@@ -38,7 +55,7 @@ def handle_message(update):
             # send the response back to Telegram
             requests.post(send_message_url, json={
                 "chat_id": chat_id,
-                "text": response["choices"][0]["text"]
+                "text": response["choices"][0]["text"].strip()
             })
             global last_update_id
             last_update_id = update["update_id"]
@@ -57,3 +74,4 @@ while True:
     else:
         print("No updates to retrieve.")
     time.sleep(5)
+    
