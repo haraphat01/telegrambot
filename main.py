2680f179e7bb7f9c6cd272c0b7d3945244132ef4
haraphat01 <haraphat@gmail.com>
2023-02-05 13:17:14 +0100

    Added to stop the loop

--- main.py
+++ main.py
@@ -36,27 +36,26 @@ def handle_message(update):
             message_text = message["text"]
             chat_id = message["chat"]["id"]
             existing_chat = collection.find_one({"chat_id": chat_id})
-            if existing_chat:
-                current_month = datetime.datetime.now().month
-                first_request_month = existing_chat.get("first_request_month")
-                num_requests = existing_chat.get("num_requests", 0)
-                if current_month != first_request_month or num_requests >= 30:
-                    # Send message indicating that the user has exceeded their monthly limit
-                    requests.post(send_message_url, json={
-                        "chat_id": chat_id,
-                        "text": "You have reached your monthly limit of 30 requests.\n Please try again next month. You can subscribe to premium service at $5 monthly or $50 yearly for unlimited request.\n You can currently pay with Etherum, Bnb, Busd and Usdt. Pay to this wallet address \n '0x983e746eDEa971338344D67E6DF755BbC37c8F76' \n and contact https://t.me/pencil_support to activate your account"
-                    })
-                    
-                    return               # Update the request count and last reset time in the database
-                collection.update_one({"chat_id": chat_id}, {"$inc": {"num_requests": 1}})
-                if first_request_month is None:
-                    collection.update_one({"chat_id": chat_id}, {"$set": {"first_request_month": current_month}})
-            else:
-                # Create a new entry for the user in the database with request count = 1
-                collection.insert_one({
+            current_month = time.strftime("%m") # get the current month
+            if existing_chat and existing_chat.get("month") == current_month and existing_chat.get("requests") >= 30:
+                requests.post(send_message_url, json={
                     "chat_id": chat_id,
-                    "first_request_month": datetime.datetime.now().month,
-                    "num_requests": 1
+                    "text": "You have used up your monthly request limit. Please try again next month."
+                })
+                return
+            if existing_chat:
+                # If it's a new month, reset the request count
+                if existing_chat.get("month") != current_month:
+                    collection.update_one({"chat_id": chat_id}, {"$set": {"month": current_month, "requests": 1}})
+                else:
+                    collection.update_one({"chat_id": chat_id}, {"$inc": {"requests": 1}})
+                
+            else:
+                # Add the user to the database for the first time
+                collection.insert_one({
+                "chat_id": chat_id, 
+                "month": current_month,
+                "requests": 1
                 })
             if  message_text.startswith("/start") or message_text.lower() == "hello":
                 message_text = "hello?"
@@ -67,7 +66,7 @@ def handle_message(update):
                 })
             model = "text-davinci-003"
             temperature = 0.7
-            max_tokens = 200
+            max_tokens = 500
             try:
                 response = openai.Completion.create(
                     model=model,
@@ -105,3 +104,7 @@ while True:
     
     
     
+    
+
+
+
